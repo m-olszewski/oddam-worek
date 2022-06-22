be5827a46d7c134bc07cedec8f254bc56656b05b
m-olszewski <michal.e.olszewski@gmail.com>
2022-06-22 22:49:55 +0200

    home header - opening

--- src/components/HomeOpening.js
+++ src/components/HomeOpening.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import '../scss/_HomeOpening.scss';
 import {Link} from "react-router-dom";
+import {ReactComponent as DecorationSvg} from '../assets/icons/Decoration.svg';
 
 const HomeOpening = () => {
     return (
-        <div className="home-opening">
-            obrazek z lewej, tekst i linki z prawej
-            <Link to="/logowanie">Oddaj rzeczy</Link>
+        <div className="home-opening container">
+            <div className="home-opening-left">
+                <img src={require('../assets/images/Home-Hero-Image.jpg')} alt="home-hero"/>
+            </div>
+            <div className="home-opening-right">
+                <h2>Zacznij pomagać!<br/>
+                    Oddaj niechciane rzeczy w zaufane ręce</h2>
+                <DecorationSvg/>
+                <div className="home-opening-right-btns">
+                    <Link to="/logowanie">
+                        <div className="home-opening-right-btn-single">Oddaj<br/>
+                            rzeczy
+                        </div>
+                    </Link>
 
+                    <div className="home-opening-right-btn-single">
+                        <Link to="/logowanie">Zorganizuj<br/>
+                            zbiórkę</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
