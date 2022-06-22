676d818a013299c7382521506553f88269702c00
m-olszewski <michal.e.olszewski@gmail.com>
2022-06-22 22:50:12 +0200

    home three columns

--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import '../scss/_HomeThreeColumns.scss';
 
+const Article = (props) => {
+    return (
+        <article className="home-three-columns-single">
+            <h1 className="home-three-columns-counter">
+                {props.counter}
+            </h1>
+            <h6 className="home-three-columns-title">
+                {props.title}
+            </h6>
+            <p className="home-three-columns-text">
+                Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.
+            </p>
+        </article>
+    );
+}
 
 const HomeThreeColumns = () => {
     return (
         <div className="home-three-columns">
-            trzykolumny
+            <Article counter="10" title="oddanych worków"/>
+            <Article counter="5" title="wspartych organizacji"/>
+            <Article counter="7" title="zorganizowanych zbiórek"/>
         </div>
     );
 };
