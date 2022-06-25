6a3227f53714bf183143d2fd44439db44d6a0001
m-olszewski <michal.e.olszewski@gmail.com>
2022-06-25 17:18:02 +0200

    aboute upd

--- src/components/HomeAbout.js
+++ src/components/HomeAbout.js
@@ -2,8 +2,7 @@ import React from 'react';
 import '../scss/_HomeAbout.scss';
 import {Element} from 'react-scroll';
 import TitleSVG from "./TitleSVG";
-import {ReactComponent as SignSvg} from "../assets/icons/Icon-4.svg";
-
+import SignSvg from "../assets/icons/Signature.svg";
 
 const HomeAbout = () => {
     return (
@@ -11,12 +10,13 @@ const HomeAbout = () => {
             <div className="home-about">
                 <div className="home-about-left">
                     <TitleSVG title1line="O nas"/>
-                    <div>
-                    <p className="home-about-text">
-                        Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts
-                        black-eyed pea prairie turnip leek lentil turnip greens parsnip.
-                    </p>
-                    <SignSvg/>
+                    <div className="home-about-left-text-container">
+                        <p className="home-about-text">
+                            Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya
+                            nuts
+                            black-eyed pea prairie turnip leek lentil turnip greens parsnip.
+                        </p>
+                        <img src={SignSvg} className="icon" alt="sign icon"/>
                     </div>
 
                 </div>
