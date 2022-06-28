b27d2ae0f3bfc6ba627e1849fce2623a2b93da4c
m-olszewski <michal.e.olszewski@gmail.com>
2022-06-28 21:25:12 +0200

    home kontakt upd

--- src/components/HomeContact.js
+++ src/components/HomeContact.js
@@ -2,36 +2,45 @@ import React from 'react';
 import {Element} from 'react-scroll';
 import '../scss/_HomeContact.scss';
 import TitleSVG from "./TitleSVG";
+import FBSvg from "../assets/icons/Facebook.svg";
+import IGSvg from "../assets/icons/Instagram.svg";
 
 const HomeContact = () => {
+
+    const handleSubmit = event => {
+        event.preventDefault();
+    };
+
     return (
         <Element name="contact">
-            <div className="home-contact container">
-                <div className="home-contact-form">
-                    <TitleSVG title1line="Skontaktuj się z nami"/>
-                    <form className="contact-form">
-                        <div className="form-up">
-                            <div className="inputbox-content">
-                                <label htmlFor="name">Wpisz swoje imię</label>
-                                <input id="name" name="name" type="text" required/>
+            <div className="home-contact-bg">
+                <div className="home-contact container">
+                    <div className="home-contact-form">
+                        <TitleSVG title1line="Skontaktuj się z nami"/>
+                        <form className="contact-form" onSubmit={handleSubmit}>
+                            <div className="form-up">
+                                <div className="inputbox-content">
+                                    <label htmlFor="name">Wpisz swoje imię</label>
+                                    <input id="name" name="name" type="text" required/>
+                                </div>
+                                <div className="inputbox-content">
+                                    <label htmlFor="email">Wpisz swój e-mail</label>
+                                    <input id="email" name="email" type="email" required/>
+                                </div>
                             </div>
-                            <div className="inputbox-content">
-                                <label htmlFor="email">Wpisz swój e-mail</label>
-                                <input id="email" name="email" type="email" required/>
+                            <div className="form-mid inputbox-content">
+                                <label htmlFor="msg">Wpisz swoją wiadomość</label>
+                                <textarea id="msg" name="msg" required/>
                             </div>
+                            <input className="form-down" type="submit" value="Wyślij"/>
+                        </form>
+                    </div>
+                    <div className="home-contact-footer">
+                        <span className="home-contact-footer-copyright">Copyright by Coders Lab</span>
+                        <div className="home-contact-footer-icons">
+                            <img src={FBSvg} alt="Facebook icon"/>
+                            <img src={IGSvg} alt="Facebook icon"/>
                         </div>
-                        <div className="form-mid inputbox-content">
-                            <label htmlFor="msg">Wpisz swoją wiadomość</label>
-                            <textarea  id="msg" name="msg" required/>
-                        </div>
-                        <input className="form-down" type="submit" value="Wyślij"/>
-                    </form>
-                </div>
-                <div className="home-contact-footer">
-                    <span className="home-contact-footer-copyright">Copyright by Coders Lab</span>
-                    <div className="home-contact-footer-icons">
-                        <svg/>
-                        <svg/>
                     </div>
                 </div>
             </div>
