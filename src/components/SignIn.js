a280d0f0b67ec20cc509b35e3b513751d1509c19
m-olszewski <michal.e.olszewski@gmail.com>
2022-07-04 17:02:09 +0200

    login/register

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,7 +3,7 @@ import '../scss/_SignIn.scss';
 
 
 const SignIn = () => {
-    const [values, setValues] = useState({email: '', password: '', password2: ''});
+    const [values, setValues] = useState({email: '', password: ''});
     const [errorMessages, setErrorMessages] = useState(null);
 
     function handleChange(event) {
@@ -21,11 +21,9 @@ const SignIn = () => {
         if (!values.email.match('[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?')) {
             errorMessages.email = 'Podany email jest nieprawidłowy!';
         }
+
         if (values.password.length < 6) {
             errorMessages.password = 'Podane hasło jest za krótkie!';
-        }
-        if (values.password.value !== values.password2.value) {
-            errorMessages.password2 = 'Podane hasła nie są identyczne';
         }
 
         return Object.keys(errorMessages).length > 0
@@ -36,19 +34,15 @@ const SignIn = () => {
     function handleSubmit(event) {
         event.preventDefault();
 
-        console.log(values.password, values.password2)
-
         const errorMessages = validate(values);
         setErrorMessages(errorMessages);
-
         console.log(errorMessages)
-
         if (errorMessages) return;
     }
 
     return (
         <div>
-            <form noValidate onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <div className="inputbox-content">
                     <label htmlFor="email">Email</label>
                     <input id="email" name="email" type="text" value={values.email} onChange={handleChange}
@@ -58,21 +52,14 @@ const SignIn = () => {
                 </div>
                 <div className="inputbox-content">
                     <label htmlFor="password">Hasło</label>
-                    <input id="password" name="password" type="password" value={values.password} onChange={handleChange}
+                    <input id="password" name="password" type="text" value={values.password} onChange={handleChange}
                            className="signin-input" required/>
                     {errorMessages?.password &&
                         <span className="error-message">{errorMessages?.password}</span>}
                 </div>
-                <div className="inputbox-content">
-                    <label htmlFor="password2">Powtórz hasło</label>
-                    <input id="password2" name="password2" type="password" value={values.password2} onChange={handleChange}
-                           className="signin-input" required/>
-                    {errorMessages?.password2 &&
-                        <span className="error-message">{errorMessages?.password2}</span>}
-                </div>
                 <div>
-                    <button value="Zaloguj się"/>
-                    <input type="submit" value="Załóż konto"/>
+                    <button value="Załóż konto"/>
+                    <input type="submit" value="Zaloguj"/>
                 </div>
             </form>
         </div>
